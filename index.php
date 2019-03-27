f2fbfd52ac1fc0f28a42d44b5caee6c62627861f
Lulz_M <yunmo159789@gmail.com>
2019-03-27 15:46:08 +0900

    a little change

--- index.php
+++ index.php
@@ -93,7 +93,7 @@
         <div class="menu-content pb-70 col-lg-8">
           <div class="title text-center">
             <h1 class="mb-10">현재 전세계 환율</h1>
-            <p>여러분들이 가야할 곳에 환율을 모르시다고요? 이곳에서 가볍게 확인하세요~</p>
+            <p>여러분들이 가야할 곳에 환율을 모르시다고요?<br>이곳에서 가볍게 확인하세요~</p>
           </div>
         </div>
       </div>
@@ -101,7 +101,7 @@
         <div class="col-lg-4">
           <div class="single-price">
             <!-- Currency Converter Script - FxExchangeRate.com  -->
-            <div style="font-size:16px;font-weight:bold;font-family:sans-serif,Arial,Helvetica;width:238px;line-height:24px;border:1px solid #2D6AB4;background-color:#FFFFFF;">
+            <div class="price-card" style="font-size:16px;font-weight:bold;font-family:sans-serif,Arial,Helvetica;width:238px;line-height:24px;border:1px solid #2D6AB4;background-color:#FFFFFF;">
               <div style="background-color:#2D6AB4;height:24px; font-weight:bold;text-align:left;padding-top:3px; width:100%;"><span style="background-image:url(https://www.fxexchangerate.com/static/flag.png); background-position: 0 -2064px;float:left; margin:4px 0 0 20px; width:20px;height:15px; background-repeat:no-repeat;"></span><a style="color:#FFFFFF;padding-left:5px;text-decoration:none;" href="https://kr.fxexchangerate.com/usd/" rel="nofollow">미국 달러</a></div>
               <script type="text/javascript" src="https://w.fxexchangerate.com/converter.php?fm=USD&ft=KRW,EUR,GBP,JPY,CHF,CAD,AUD,CNY,HKD,RUB,MXN,&lg=kr&am=1&ty=2"></script>
             </div>
@@ -111,7 +111,7 @@
         <div class="col-lg-4">
           <div class="single-price">
             <!-- Currency Converter Script - FxExchangeRate.com  -->
-            <div style="font-size:16px;font-weight:bold;font-family:sans-serif,Arial,Helvetica;width:238px; line-height:24px;border:1px solid #2D6AB4;background-color:#FFFFFF;">
+            <div class="price-card" style="font-size:16px;font-weight:bold;font-family:sans-serif,Arial,Helvetica;width:238px; line-height:24px;border:1px solid #2D6AB4;background-color:#FFFFFF;">
               <div style="background-color:#2D6AB4;height:24px; font-weight:bold;text-align:left;padding-top:3px; width:100%;"><span style="background-image:url(https://www.fxexchangerate.com/static/flag.png); background-position: 0 -576px;float:left; margin:4px 0 0 20px; width:20px;height:15px; background-repeat:no-repeat;"></span><a rel="nofollow" style="color:#FFFFFF;padding-left:5px;text-decoration:none;" href="https://kr.fxexchangerate.com/eur/">유로</a></div>
               <script type="text/javascript" src="https://w.fxexchangerate.com/converter.php?fm=EUR&ft=KRW,USD,GBP,JPY,CHF,CAD,AUD,CNY,HKD,RUB,MXN,&lg=kr&am=1&ty=2"></script>
             </div><!--  End of Currency Converter Script -  FxExchangeRate.com -->
@@ -120,7 +120,7 @@
         <div class="col-lg-4">
           <div class="single-price">
             <!-- Currency Converter Script - FxExchangeRate.com  -->
-            <div style="font-size:16px;font-weight:bold;font-family:sans-serif,Arial,Helvetica;width:238px; line-height:24px;border:1px solid #2D6AB4;background-color:#FFFFFF;">
+            <div class="price-card" style="font-size:16px;font-weight:bold;font-family:sans-serif,Arial,Helvetica;width:238px; line-height:24px;border:1px solid #2D6AB4;background-color:#FFFFFF;">
               <div style="background-color:#2D6AB4;height:24px; font-weight:bold;text-align:left;padding-top:3px; width:100%;"><span style="background-image:url(https://www.fxexchangerate.com/static/flag.png); background-position: 0 -368px;float:left; margin:4px 0 0 20px; width:20px;height:15px; background-repeat:no-repeat;"></span><a rel="nofollow" style="color:#FFFFFF;padding-left:5px;text-decoration:none;" href="https://kr.fxexchangerate.com/cny/">중국 위안</a></div>
               <script type="text/javascript" src="https://w.fxexchangerate.com/converter.php?fm=CNY&ft=KRW,USD,EUR,GBP,JPY,CHF,CAD,AUD,HKD,RUB,MXN,&lg=kr&am=1&ty=2"></script>
             </div><!--  End of Currency Converter Script -  FxExchangeRate.com -->
