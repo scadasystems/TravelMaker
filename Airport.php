3e4de0eb61d5658929ade174e2ee27293b5f6173
whale14 <zzazzu14@gmail.com>
2019-04-02 17:35:42 +0900

    airline top20 list, pagination 껍데기

--- Airport.php
+++ Airport.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html class="no-js">
 <!-- Head -->
+<link rel="stylesheet" href="css/airport.css">
 <?php
    include 'commonPHP/commonHead.php'
 ?>
@@ -26,7 +27,107 @@
     </div>
   </section>
   <!-- End banner Area -->
+
+  <!-- star list Area -->
+  <div class="container">
+    <div class="text" style="margin-left:0; margin-top:50px">
+      <p style="margin-left:0">Airline Top20</p>
+    </div>
+    <div id="item-list">
+      <ul>
+        <li>
+          <a class="expand_airline">
+            <div class="right-arrow">+</div>
+            <div class="icon_airline london"></div>
+            <h2>London</h2>
+            <span>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy</span>
+          </a>
+
+          <div class="detail">
+            <div><span>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</span></div><br />
+
+            <span class="button">Connect</span>
+          </div>
+        </li>
+        <li>
+          <a class="expand_airline">
+            <div class="right-arrow">+</div>
+            <div class="icon_airline newyork"></div>
+            <h2>New York</h2>
+            <span>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat</span>
+          </a>
+
+          <div class="detail">
+            <div><span>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</span></div><br />
+
+            <span class="button">Connect</span>
+          </div>
+        </li>
+        <li>
+          <a class="expand_airline">
+            <div class="right-arrow">+</div>
+            <div class="icon_airline paris"></div>
+            <h2>Paris</h2>
+            <span>Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum</span>
+          </a>
+
+          <div class="detail">
+            <div><span>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</span></div><br />
+
+            <span class="button">Connect</span>
+          </div>
+        </li>
+        <li>
+          <a class="expand_airline">
+            <div class="right-arrow">+</div>
+            <div class="icon_airline newyork"></div>
+            <h2>New York</h2>
+            <span>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat</span>
+          </a>
+
+          <div class="detail">
+            <div><span>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</span></div><br />
+
+            <span class="button">Connect</span>
+          </div>
+        </li>
+        <li>
+          <a class="expand_airline">
+            <div class="right-arrow">+</div>
+            <div class="icon_airline newyork"></div>
+            <h2>New York</h2>
+            <span>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat</span>
+          </a>
+
+          <div class="detail">
+            <div><span>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</span></div><br />
+
+            <span class="button">Connect</span>
+          </div>
+        </li>
+
+      </ul>
+    </div>
+    <nav class="pagination-container">
+      <div class="pagination">
+        <a class="pagination-newer" href="#">PREV</a>
+        <span class="pagination-inner">
+          <a class="pagination-active" href="#">1</a>
+          <a href="#">2</a>
+          <a href="#">3</a>
+          <a href="#">4</a>
+          <a href="#">5</a>
+        </span>
+        <a class="pagination-older" href="#">NEXT</a>
+      </div>
+    </nav>
+  </div>
   
+  <script>
+    
+
+  </script>
+  <!-- End list Area -->
   <!-- start footer Area -->
   <?php
       include 'commonPHP/footer.php';
@@ -47,6 +148,8 @@
   <script src="js/owl.carousel.min.js"></script>
   <script src="js/mail-script.js"></script>
   <script src="js/main.js"></script>
+  <script src="js/airport.js"></script>
+  
 </body>
 
 </html>
