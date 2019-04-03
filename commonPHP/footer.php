8cde7e48ce8711661ff77d072a63814d5771a76a
Lulz_M <yunmo159789@gmail.com>
2019-04-03 15:14:46 +0900

    에러 확인

--- commonPHP/footer.php
+++ commonPHP/footer.php
@@ -1,80 +1,80 @@
 <footer class="footer-area section-gap">
-      <div class="container">
+  <div class="container">
 
-         <div class="row">
-            <div class="col-lg-3  col-md-6 col-sm-6">
-               <div class="single-footer-widget">
-                  <h6>About</h6>
-                  <p>
-                     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                  </p>
-               </div>
-            </div>
-            <div class="col-lg-3 col-md-6 col-sm-6">
-               <div class="single-footer-widget">
-                  <h6>Navigation Links</h6>
-                  <div class="row">
-                     <div class="col">
-                        <ul>
-                           <li><a href="../index.php">Home</a></li>
-                           <li><a href="../Hotel.php">호텔</a></li>
-                           <li><a href="../experience.php">경험</a></li>
-                           <li><a href="../Food.php">음식점</a></li>
-                        </ul>
-                     </div>
-                     <div class="col">
-                        <ul>
-                           <li><a href="../Airport.php">항공사</a></li>
-                           <li><a href="#">2019 베스트</a></li>
-                           <li><a href="#">Help Center</a></li>
-                           <li><a href="#">Contact</a></li>
-                        </ul>
-                     </div>
-                  </div>
-               </div>
-            </div>
-            <div class="col-lg-3  col-md-6 col-sm-6">
-               <div class="single-footer-widget">
-                  <h6>Newsletter</h6>
-                  <p>
-                     새로운 정보를 원하시는 분들은 아래에 이메일 주소를 적어주세요. <br>저희가 따끈한 신상들을 보내드릴게요!
-                  </p>
-                  <div id="mc_embed_signup">
-                     <form target="_blank" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get" class="subscription relative">
-                        <div class="input-group d-flex flex-row">
-                           <input name="EMAIL" placeholder="Email Address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email Address '" required="" type="email">
-                           <button class="btn bb-btn"><span class="lnr lnr-location"></span></button>
-                        </div>
-                        <div class="mt-10 info"></div>
-                     </form>
-                  </div>
-               </div>
-            </div>
-            <div class="col-lg-3  col-md-6 col-sm-6">
-               <div class="single-footer-widget mail-chimp">
-                  <h6 class="mb-20">Developers</h6>
-                  <ul>
-                     <li><img src="img/our_team.jpg" alt="" style="width: 250xp; height: 130px"></li>
-                  </ul>
-               </div>
-            </div>
-         </div>
-
-         <div class="row footer-bottom d-flex justify-content-between align-items-center">
-            <p class="col-lg-8 col-sm-12 footer-text m-0">
-               <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-               Copyright &copy;<script>
-                  document.write(new Date().getFullYear());
-
-               </script> All rights reserved | This Site is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by Travel Maker</a>
-               <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-            </p>
-            <div class="col-lg-4 col-sm-12 footer-social">
-               <a href="#"><i class="fa fa-facebook"></i></a>
-               <a href="#"><i class="fa fa-twitter"></i></a>
-               <a href="#"><i class="fa fa-instagram"></i></a>
-               <a href="https://github.com/scadasystems/TravelMaker" target="_blank"><i class="fa fa-github"></i></a>
-            </div>
-         </div>
+    <div class="row">
+      <div class="col-lg-3  col-md-6 col-sm-6">
+        <div class="single-footer-widget">
+          <h6>About</h6>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+        </div>
       </div>
-   </footer>
+      <div class="col-lg-3 col-md-6 col-sm-6">
+        <div class="single-footer-widget">
+          <h6>Navigation Links</h6>
+          <div class="row">
+            <div class="col">
+              <ul>
+                <li><a href="../index.php">Home</a></li>
+                <li><a href="../Hotel.php">호텔</a></li>
+                <li><a href="../experience.php">경험</a></li>
+                <li><a href="../Food.php">음식점</a></li>
+              </ul>
+            </div>
+            <div class="col">
+              <ul>
+                <li><a href="../Airport.php">항공사</a></li>
+                <li><a href="#">2019 베스트</a></li>
+                <li><a href="#">Help Center</a></li>
+                <li><a href="#">Contact</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3  col-md-6 col-sm-6">
+        <div class="single-footer-widget">
+          <h6>Newsletter</h6>
+          <p>
+            새로운 정보를 원하시는 분들은 아래에 이메일 주소를 적어주세요. <br>저희가 따끈한 신상들을 보내드릴게요!
+          </p>
+          <div id="mc_embed_signup">
+            <form target="_blank" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get" class="subscription relative">
+              <div class="input-group d-flex flex-row">
+                <input name="EMAIL" placeholder="Email Address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email Address '" required="" type="email">
+                <button class="btn bb-btn"><span class="lnr lnr-location"></span></button>
+              </div>
+              <div class="mt-10 info"></div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3  col-md-6 col-sm-6">
+        <div class="single-footer-widget mail-chimp">
+          <h6 class="mb-20">Developers</h6>
+          <ul>
+            <li><img src="img/our_team.jpg" alt="" style="width: 250xp; height: 130px"></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <div class="row footer-bottom d-flex justify-content-between align-items-center">
+      <p class="col-lg-8 col-sm-12 footer-text m-0">
+        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+        <a>Copyright &copy;<script>
+            document.write(new Date().getFullYear());
+
+          </script> All rights reserved | This Site is made with <i class="fa fa-heart-o" aria-hidden="true"></i>by Travel Maker</a>
+        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+      </p>
+      <div class="col-lg-4 col-sm-12 footer-social">
+        <a href="#"><i class="fa fa-facebook"></i></a>
+        <a href="#"><i class="fa fa-twitter"></i></a>
+        <a href="#"><i class="fa fa-instagram"></i></a>
+        <a href="https://github.com/scadasystems/TravelMaker" target="_blank"><i class="fa fa-github"></i></a>
+      </div>
+    </div>
+  </div>
+</footer>
