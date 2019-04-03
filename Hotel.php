5cf7b124f3f1c959129fbdeeb4899b5a840c8875
Lulz_M <yunmo159789@gmail.com>
2019-04-03 16:22:52 +0900

    Hotel update

--- Hotel.php
+++ Hotel.php
@@ -3,14 +3,13 @@
 <!-- Head -->
 <?php
    include 'commonPHP/commonHead.php'
-?> 
+?>
 
 <body>
   <?php
    include 'commonPHP/Header.php'
   ?>
   <!-- end Header -->
-
   <!-- start banner Area -->
   <section class="about-banner relative">
     <div class="overlay overlay-bg"></div>
@@ -26,7 +25,6 @@
     </div>
   </section>
   <!-- End banner Area -->
-
   <!--================ Start banner section =================-->
   <section class="home-banner-area relative">
     <div class="container-fluid">
@@ -55,7 +53,6 @@
             <img src="img/next.png" alt="">
           </a>
         </div>
-
         <div class="col-lg-6 col-md-6 header-left">
           <div class="">
             <img class="img-fluid w-100" src="img/banner/Tulemar%20Bungalows.jpg">
@@ -98,20 +95,234 @@
               <div class="content__main">
                 <p>“완벽한 휴가. 가장 좋았던 부분은 그저 베란다에서 쉬면서 바다를 바라보고 날아가는 앵무새를 보는 것이었습니다.” </p>
                 <!-- 버튼 넣기 -->
-                <p><button class="btn-hotel">이동하기</button></p>
+                <p><button class="btn-hotel">호텔 웹사이트 방문</button></p>
               </div>
               <h3 class="content__index">01</h3>
             </div>
           </div>
         </div>
-        <!-- 베스트 호텔 top 20-->
-        
-        
+        <div class="spinner__face" data-bg="#19304a">
+          <div class="content" data-type="belvedere">
+            <div class="content__left">
+              <h1>Hotel Belvedere<br><span>리치오네, 이탈리아</span></h1>
+            </div>
+            <div class="content__right">
+              <div class="content__main">
+                <p>
+                  “환상적인 분위기, 훌륭한 직원들, 좋은 가이드는 물론 자전거도 즐길 수 있어 계속 다시 찾고 싶어요. 냉정한 비평가마저 만족시킬 수 있는 것이 성공의 공식이죠.”
+                </p>
+                <p><button class="btn-hotel">호텔 웹사이트 방문</button></p>
+              </div>
+              <h3 class="content__index">02</h3>
+            </div>
+          </div>
+        </div>
+        <div class="spinner__face" data-bg="#2b2533">
+          <div class="content" data-type="viroth">
+            <div class="content__left">
+              <h1>Viroth's Hotel<br><span>시엠립, 캄보디아</span></h1>
+            </div>
+            <div class="content__right">
+              <div class="content__main">
+                <p>
+                  “세련되고 고요한, 푸르고 럭셔리한 오아시스. 친절하게 도움을 주면서도 지나치지 않은 서비스. 비로스는 장엄한 사원을 볼 수 있는 멋진 장소에 그치지 않고 그 자체로도 갈 만한 곳입니다.”
+                </p>
+                <p><button class="btn-hotel">호텔 웹사이트 방문</button></p>
+              </div>
+              <h3 class="content__index">03</h3>
+            </div>
+          </div>
+        </div>
+        <div class="spinner__face" data-bg="#312f2d">
+          <div class="content" data-type="kenting">
+            <div class="content__left">
+              <h1>Kenting Amanda Hotel<br><span>Hengchun, 핑둥</span></h1>
+            </div>
+            <div class="content__right">
+              <div class="content__main">
+                <p>“...”</p>
+                <p><button class="btn-hotel">호텔 웹사이트 방문</button></p>
+              </div>
+              <h3 class="content__index">04</h3>
+            </div>
+          </div>
+        </div>
+        <div class="spinner__face" data-bg="#312f2d">
+          <div class="content" data-type="tux">
+            <div class="content__left">
+              <h1>Hotel Alpin Spa Tuxerhof<br><span>턱스, 오스트리아</span></h1>
+            </div>
+            <div class="content__right">
+              <div class="content__main">
+                <p>“...”</p>
+                <p><button class="btn-hotel">호텔 웹사이트 방문</button></p>
+              </div>
+              <h3 class="content__index">05</h3>
+            </div>
+          </div>
+        </div>
+        <div class="spinner__face" data-bg="#312f2d">
+          <div class="content" data-type="inn">
+            <div class="content__left">
+              <h1>French Quarter Inn<br><span>찰스턴, 사우스캐롤라이나</span></h1>
+            </div>
+            <div class="content__right">
+              <div class="content__main">
+                <p>“위치는 편리하고 객실은 깔끔하며 와인, 치즈, 쿠키를 비롯한 다른 서프라이즈 역시 훌륭합니다. 베개도 직접 선택할 수 있었고요."</p>
+                <p><button class="btn-hotel">호텔 웹사이트 방문</button></p>
+              </div>
+              <h3 class="content__index">06</h3>
+            </div>
+          </div>
+        </div>
+        <div class="spinner__face" data-bg="#312f2d">
+          <div class="content" data-type="pedregal">
+            <div class="content__left">
+              <h1>The Resort at Pedregal<br><span>카보 산 루카스, 멕시코</span></h1>
+            </div>
+            <div class="content__right">
+              <div class="content__main">
+                <p>“..."</p>
+                <p><button class="btn-hotel">호텔 웹사이트 방문</button></p>
+              </div>
+              <h3 class="content__index">07</h3>
+            </div>
+          </div>
+        </div>
+        <div class="spinner__face" data-bg="#312f2d">
+          <div class="content" data-type="palacio">
+            <div class="content__left">
+              <h1>Belmond Palacio Nazarenas<br><span>쿠스코, 페루</span></h1>
+            </div>
+            <div class="content__right">
+              <div class="content__main">
+                <p>“..."</p>
+                <p><button class="btn-hotel">호텔 웹사이트 방문</button></p>
+              </div>
+              <h3 class="content__index">08</h3>
+            </div>
+          </div>
+        </div>
+        <div class="spinner__face" data-bg="#312f2d">
+          <div class="content" data-type="caves">
+            <div class="content__left">
+              <h1>Kayakapi Premium Caves<br><span>위르귀프, 터키</span></h1>
+            </div>
+            <div class="content__right">
+              <div class="content__main">
+                <p>
+                  “최고입니다!!!”<br>
+                  “완벽한 호텔”
+                </p>
+                <p><button class="btn-hotel">호텔 웹사이트 방문</button></p>
+              </div>
+              <h3 class="content__index">09</h3>
+            </div>
+          </div>
+        </div>
+        <div class="spinner__face" data-bg="#312f2d">
+          <div class="content" data-type="siesta">
+            <div class="content__left">
+              <h1>Hanoi La Siesta Hotel<br><span>하노이, 베트남</span></h1>
+            </div>
+            <div class="content__right">
+              <div class="content__main">
+                <p>
+                  “서비스, 객실 그리고 음식에 완전히 반했습니다. 평가에서 가능하면 10개라도 별을 주고 싶네요.”
+                </p>
+                <p><button class="btn-hotel">호텔 웹사이트 방문</button></p>
+              </div>
+              <h3 class="content__index">10</h3>
+            </div>
+          </div>
+        </div>
+        <div class="spinner__face" data-bg="#312f2d">
+          <div class="content" data-type="retreat">
+            <div class="content__left">
+              <h1>Golden Temple Retreat<br><span>시엠립, 캄보디아</span></h1>
+            </div>
+            <div class="content__right">
+              <div class="content__main">
+                <p>
+                  “Amazing hotel ever ”
+                  “만족만족만족”
+                </p>
+                <p><button class="btn-hotel">호텔 웹사이트 방문</button></p>
+              </div>
+              <h3 class="content__index">11</h3>
+            </div>
+          </div>
+        </div>
+        <div class="spinner__face" data-bg="#312f2d">
+          <div class="content" data-type="12">
+            <div class="content__left">
+              <h1>Quinta Jardins do Lago<br><span>푼샬, 포르투갈</span></h1>
+            </div>
+            <div class="content__right">
+              <div class="content__main">
+                <p>
+                  “...”
+                </p>
+                <p><button class="btn-hotel">호텔 웹사이트 방문</button></p>
+              </div>
+              <h3 class="content__index">12</h3>
+            </div>
+          </div>
+        </div>
+        <div class="spinner__face" data-bg="#312f2d">
+          <div class="content" data-type="13">
+            <div class="content__left">
+              <h1>The Oberoi Rajvilas<br><span>자이푸르, 인도</span></h1>
+            </div>
+            <div class="content__right">
+              <div class="content__main">
+                <p>
+                  “...”
+                </p>
+                <p><button class="btn-hotel">호텔 웹사이트 방문</button></p>
+              </div>
+              <h3 class="content__index">13</h3>
+            </div>
+          </div>
+        </div>
+        <div class="spinner__face" data-bg="#312f2d">
+          <div class="content" data-type="14">
+            <div class="content__left">
+              <h1>Hotel Amira Istanbul<br><span>이스탄불, 터키</span></h1>
+            </div>
+            <div class="content__right">
+              <div class="content__main">
+                <p>
+                  “마주쳤던 모든 직원이 친절했고 잘 응대해주었어요. 옥상 데크는 평화로운 오아시스 같은 곳이구요. 와인 한 잔을 즐기면서 아름다운 마르마라해를 즐기기에 완벽한 곳입니다.”
+                </p>
+                <p><button class="btn-hotel">호텔 웹사이트 방문</button></p>
+              </div>
+              <h3 class="content__index">14</h3>
+            </div>
+          </div>
+        </div>
+        <div class="spinner__face" data-bg="#312f2d">
+          <div class="content" data-type="15">
+            <div class="content__left">
+              <h1>Hotel 41<br><span>런던, 영국</span></h1>
+            </div>
+            <div class="content__right">
+              <div class="content__main">
+                <p>
+                  “숙박과 서비스 모두 훌륭합니다.”<br>
+                  “놀라운 서비스 덕에 좋은 시간을 보냈습니다!!! ”
+                </p>
+                <p><button class="btn-hotel">호텔 웹사이트 방문</button></p>
+              </div>
+              <h3 class="content__index">15</h3>
+            </div>
+          </div>
+        </div>
+        <!-- end Best Hotel 15-->
       </div>
     </div>
   </div>
   <!-- end slider -->
-
   <!-- start footer Area -->
   <?php
       include 'commonPHP/footer.php';
@@ -132,7 +343,6 @@
   <script src="js/owl.carousel.min.js"></script>
   <script src="js/mail-script.js"></script>
   <script src="js/main.js"></script>
-
 </body>
 
 </html>
